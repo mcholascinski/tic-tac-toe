1ce18334f8eb3ca8f8cfe3cfe85b426c78a6a463
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-07-06 22:49:41 +0200

    Two players logic first version

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ const game = document.querySelector(".game");
 // all cells on the game board
 const cells = document.querySelectorAll(".cell");
 // restart game button
+const playerX = document.querySelector("#x-player");
+const playerO = document.querySelector("#o-player");
+const xPoints = document.querySelector("#x-points");
+const oPoints = document.querySelector("#o-points");
+const tiesPoints = document.querySelector("#ties-points");
 const restartBtn = document.querySelector("#restart");
 const modalBackground = document.querySelector(".modal-background");
 const modal = document.querySelector(".modal");
 const modalSmallHeader = document.querySelector("#modal-small-header");
 const modalLargeHeader = document.querySelector("#modal-large-header");
+const modalLargeHeaderText = document.querySelector("#modal-large-header-text");
 const winnerIcon = document.querySelector("#winner-icon");
 const modalBtnOne = document.querySelector("#modal-btn-one");
 const modalBtnTwo = document.querySelector("#modal-btn-two");
+const modalBtnOneRestart = document.querySelector("#modal-btn-one-restart");
+const modalBtnTwoRestart = document.querySelector("#modal-btn-two-restart");
 
 // all possible wins
 const possibleWins = [
@@ -38,23 +46,80 @@ let playerOne;
 let chosenPlayer;
 
 function showModalWin() {
+    modalBtnOneRestart.style.display = "none";
+    modalBtnTwoRestart.style.display = "none";
+    modalBtnOne.style.display = "inline";
+    modalBtnTwo.style.display = "inline";
     modalBackground.style.display = "block";
     modal.style.display = "flex";
     modalBtnOne.textContent = "Quit";
     modalBtnTwo.textContent = "Next Round";
-    currentUser === playerOne ? modalSmallHeader.textContent = "Player 1 wins!" : modalSmallHeader.textContent = "Player 2 wins!";
+    currentUser === playerOne ? modalSmallHeader.innerHTML = "Player 1 wins!" : modalSmallHeader.innerHTML = "Player 2 wins!";
     winnerIcon.innerHTML = currentUser === "X" ? `<img src="./assets/icon-x.svg" alt=""></img>` : `<img src="./assets/icon-o.svg" alt=""></img>`;
-    modalLargeHeader.classList = currentUser === "X" ? "blue-text" : "yellow-text";
+    modalLargeHeaderText.innerHTML = "takes the round";
+    modalLargeHeaderText.classList = currentUser === "X" ? "blue-text" : "yellow-text";
     modalBtnOne.addEventListener("click", () => {
         modalBackground.style.display = "none";
         modal.style.display = "none";
         game.style.display = "none";
         newGame.style.display = "block";
+        chooseX.classList.add("mark-btn-light");
+        chooseO.classList.remove("mark-btn-light");
     })
     modalBtnTwo.addEventListener("click", () => {
         modalBackground.style.display = "none";
         modal.style.display = "none";
         startGame("next", playerOne);
+    })
+}
+
+function showModalTie() {
+    modalBtnOneRestart.style.display = "none";
+    modalBtnTwoRestart.style.display = "none";
+    modalBtnOne.style.display = "inline";
+    modalBtnTwo.style.display = "inline";
+    modalBackground.style.display = "block";
+    modal.style.display = "flex";
+    modalBtnOne.textContent = "Quit";
+    modalBtnTwo.textContent = "Next Round";
+    modalLargeHeaderText.textContent = "Round tied";
+    modalLargeHeaderText.classList = "silver-text";
+    winnerIcon.innerHTML = null;
+    modalBtnOne.addEventListener("click", () => {
+        modalBackground.style.display = "none";
+        modal.style.display = "none";
+        game.style.display = "none";
+        newGame.style.display = "block";
+        chooseX.classList.add("mark-btn-light");
+        chooseO.classList.remove("mark-btn-light");
+    })
+    modalBtnTwo.addEventListener("click", () => {
+        modalBackground.style.display = "none";
+        modal.style.display = "none";
+        startGame("next", playerOne);
+    })
+}
+
+function showModalRestart() {
+    modalBtnOneRestart.style.display = "inline";
+    modalBtnTwoRestart.style.display = "inline";
+    modalBtnOne.style.display = "none";
+    modalBtnTwo.style.display = "none";
+    modalBackground.style.display = "block";
+    modal.style.display = "flex";
+    modalBtnOneRestart.textContent = "No, cancel";
+    modalBtnTwoRestart.textContent = "Yes, restart";
+    modalLargeHeaderText.textContent = "Restart game?";
+    modalLargeHeaderText.classList = "silver-text";
+    winnerIcon.innerHTML = null;
+    modalBtnOneRestart.addEventListener("click", () => {
+        modalBackground.style.display = "none";
+        modal.style.display = "none";
+    })
+    modalBtnTwoRestart.addEventListener("click", () => {
+        modalBackground.style.display = "none";
+        modal.style.display = "none";
+        startGame("new", playerOne);
     })
 }
 
@@ -77,9 +142,9 @@ function move(clickedCellId) {
 
 function isDraw() {
     if (possibleMoves.length === 0) {
-        console.log("draw!")
-        scoreX++;
-        scoreO++;
+        ties++;
+        tiesPoints.textContent = ties;
+        showModalTie();
     } else {
         currentUser === "X" ? currentUser = "O" : currentUser = "X";
     }
@@ -95,6 +160,8 @@ function isWinner(currentUserMoves) {
     if (isWinningMove.isWinning) {
         isWinningMove.winningArray.forEach((e) => cells[e].classList.add("cell-win"));
         currentUser === "X" ? scoreX++ : scoreO++;
+        xPoints.textContent = scoreX;
+        oPoints.textContent = scoreO;
         showModalWin();
     } else {
         isDraw();
@@ -116,18 +183,29 @@ function removeHover(cell) {
 }
 
 function startGame(state, player) {
-    playerOne = player;
+    player == null ? playerOne = "X" : playerOne = player;
     possibleMoves = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
     userXMoves = [];
     userOMoves = [];
     currentUser = "X";
+    if (playerOne === "X") {
+        playerX.textContent = "P1";
+        playerO.textContent = "P2";
+    } else {
+        playerX.textContent = "P2";
+        playerO.textContent = "P1";
+    }
     cells.forEach((cell) => {
         cell.innerHTML = "";
         cell.classList.remove("cell-win");
     });
     if (state === "new") {
+        ties = 0;
         scoreX = 0;
         scoreO = 0;
+        tiesPoints.textContent = ties;
+        xPoints.textContent = scoreX;
+        oPoints.textContent = scoreO;
     }
 }
 
@@ -162,6 +240,4 @@ cells.forEach((cell) => {
     });
   });
 
-restartBtn.addEventListener("click", () => {
-    startGame("new", chosenPlayer);
-  });
+restartBtn.addEventListener("click", showModalRestart);
